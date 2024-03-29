19e289f60ab5c715b0464207c804f08c898fe679
LifeValuable <nikish0715@yandex.ru>
2024-03-29 19:36:13 +0300

    GroupAnagrams:

---
+++ trees/groupAnagrams.go
@@ -0,0 +1,40 @@
+package trees
+
+import (
+	"sort"
+)
+
+type runesSlice []rune
+
+func (s runesSlice) Less(i, j int) bool {
+	return s[i] < s[j]
+}
+
+func (s runesSlice) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s runesSlice) Len() int {
+	return len(s)
+}
+
+func SortString(s string) string {
+	r := []rune(s)
+	sort.Sort(runesSlice(r))
+	return string(r)
+}
+
+func groupAnagrams(strs []string) [][]string {
+	groups := make(map[string][]string)
+	for _, str := range strs {
+		sorted_str := SortString(str)
+		groups[sorted_str] = append(groups[sorted_str], str)
+	}
+	result := make([][]string, len(groups))
+	for _, arr := range groups {
+		if len(arr) > 0 {
+			result = append(result, arr)
+		}
+	}
+	return result
+}
